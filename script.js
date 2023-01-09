5c3220e38b8cd22834efd7d5cd022743c99b3404
mustafaumar2002@gmail.com <mustafaumar2002@gmail.com>
2023-01-09 07:26:11 +0100

    Add first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navlinklist = document.querySelector(".nav_center");
+
+
+
+hamburger.addEventListener("click", () => {
+    navlinklist.classList.toggle("actived");
+    hamburger.classList.toggle("active");
+
+
+})
+
+document.querySelectorAll(".nav--links").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navlinklist.classList.remove("active");
+}))
